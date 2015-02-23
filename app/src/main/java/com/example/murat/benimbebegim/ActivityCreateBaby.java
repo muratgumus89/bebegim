f9bb59297ef5567ec9b9c6495500573fbf1b294c
ayttunc <aytunc.matrac@hotmail.com.tr>
2015-02-23 20:04:47 +0200

    muratım

--- app/src/main/java/com/example/murat/benimbebegim/ActivityCreateBaby.java
+++ app/src/main/java/com/example/murat/benimbebegim/ActivityCreateBaby.java
@@ -11,5 +11,6 @@ public class ActivityCreateBaby extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_baby);
+        //murat degisiklik yaptım
     }
 }
